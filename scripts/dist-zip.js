6acf88f91c88ef2d02a3e484681fa9ad8911b248
Sven Grunewaldt <strayer@olle-orks.org>
2016-01-23 19:53:10 +0100

    Add dist npm script to create dist zip

---
+++ scripts/dist-zip.js
@@ -0,0 +1,15 @@
+var file_system = require('fs');
+var archiver = require('archiver');
+
+var output = file_system.createWriteStream("build/dist-" + process.env.npm_package_version + ".zip");
+var archive = archiver.create('zip', {});
+archive.pipe(output);
+
+//archive.directory("build", "favicon_unread_counter");
+archive.file("build/favicon-unread-count.min.js", {
+    "name": "favicon_unread_counter/favicon-unread-count.min.js"
+});
+archive.file("build/init.php", {
+    "name": "favicon_unread_counter/init.php"
+});
+archive.finalize();
